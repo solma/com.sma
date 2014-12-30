a9470b10ddf880cca830a80df29a0548e076fea9
solma <solma@motorola.com>
2014-12-30 15:07:09 -0800

    update-CountingInversion

--- src/com/shuoma/alg/number/CountingInversion.java
+++ src/com/shuoma/alg/number/CountingInversion.java
@@ -8,14 +8,15 @@ import java.util.Arrays;
 import java.util.List;
 
 
-// Having a int array A[], generate another int array B[], B[i] is the count of elements in A[i+1] ~
-// A[n-1] which
-// is smaller than A[i]. Time complexity: O(nlgn)
+/*
+ *  Having a int array A[], generate another int array B[], B[i] is the count of elements
+ *  in A[i+1] ~ A[n-1] which is smaller than A[i]. Time complexity: O(nlgn)
+ */
 public class CountingInversion {
   public static void main(String[] args) {
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 10; i++) {
       //int [] arr = {3, 4, 0, 2, 1};
-      int[] arr = RandomUtil.shuffle(ArrayUtil.getNaturalArray(RandomUtil.r.nextInt(10) + 15));
+      int[] arr = RandomUtil.shuffle(ArrayUtil.getNaturalArray(RandomUtil.r.nextInt(30) + 15));
       //System.out.println(Arrays.toString(arr));
       int[][] res = new int[2][arr.length];
       res[0] = countInversion(arr);
@@ -51,41 +52,29 @@ public class CountingInversion {
     public BSTWithSize(int[] keys) {
       result = new ArrayList<>();
       for (int i : ArrayUtil.reverse(Arrays.copyOf(keys, keys.length))) {
-        insert(root, i, 0);
+        root = insert(root, i, 0);
       }
     }
 
-    void insert(BSTNodeWithSize cur, int key, int smallerNodeCnt) {
-      if (cur == null) {
-        root = new BSTNodeWithSize(key, smallerNodeCnt, result);
-      } else {
-        if (cur.value >= key) {
-          if (cur.left != null) {
-            insert(cur.left, key, smallerNodeCnt);
-          } else {
-            cur.left = new BSTNodeWithSize(key, smallerNodeCnt, result);
-          }
-          cur.leftSize++;
-        } else {
-          if (cur.right != null) {
-            insert(cur.right, key, smallerNodeCnt + cur.leftSize + 1);
-          } else {
-            cur.right = new BSTNodeWithSize(key, smallerNodeCnt + cur.leftSize + 1, result);
-          }
-        }
-      }
+    BSTNodeWithSize insert(BSTNodeWithSize cur, int key, int smallerNodeCnt) {
+      if (cur == null) return new BSTNodeWithSize(key, smallerNodeCnt, result);
+      if (cur.value >= key) cur.left = insert(cur.left, key, smallerNodeCnt);
+      else cur.right = insert(cur.right, key, smallerNodeCnt + ((cur.left == null) ? 0 : cur.left.size) + 1);
+      cur.size++;
+      return cur;
     }
   }
 
   public static class BSTNodeWithSize {
     int value;
-    int leftSize; // size of the left child tree
+    int size;
     int smallerItemCnt;
     BSTNodeWithSize parent, left, right;
 
     public BSTNodeWithSize(int value, int smallerNodeCnt, List<Integer> res) {
       this.value = value;
       this.smallerItemCnt = smallerNodeCnt;
+      this.size = 1;
       res.add(0, smallerNodeCnt);
     }
 
