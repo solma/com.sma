3fd630c2004c9bd7f214ce13b6a8791aef1f4c6a
solma <solma@motorola.com>
2015-01-27 15:39:52 -0800

    minor reformatting

--- src/com/shuoma/alg/streaming/SlidingWindowStats.java
+++ src/com/shuoma/alg/streaming/SlidingWindowStats.java
@@ -6,68 +6,86 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 
-/** Given a stream, compute the stats, e.g. average, min of a sliding window. */
+/**
+ * Given a stream, compute the stats, e.g. average, min of a sliding window.
+ */
 public class SlidingWindowStats {
-  public static void main(String[] args) {
-    int length = 100, K = 3;
-    int[] ary = RandomUtil.genRandomArray(length, 10, true, false);
-    SlidingWindowStats ins = new SlidingWindowStats(K, ary);
+    public static void main(String[] args) {
+        int length = 100, K = 3;
+        int[] ary;
 
-    System.out.println(Arrays.toString(ary));
-    System.out.println("\t" + Arrays.toString(ins.average()));
-    System.out.println("\t" + Arrays.toString(ins.max()));
-  }
-  
-  /** Window size. */
-  int K;  
-  /** Array that simulates a stream. */
-  int[] stream;
+        ary = RandomUtil.genRandomArray(length, 10, true, false);
+        ary = new int[] {1, 4, 8, 9, 10, 5, 5, 10, 8, 5};
 
-  /**
-   * @param k  Window size.
-   * @param stream Array that simulates a stream.
-   */
-  public SlidingWindowStats(int k, int[] stream) {
-    this.K = k;
-    this.stream = stream;
-  }
+        SlidingWindowStats ins = new SlidingWindowStats(K, ary);
 
-  /** Get the average of the sliding window. */
-  public double[] average() {
-    double sum = 0;
-    double[] avg = new double[stream.length - K + 1];
-    for (int i = 0; i < stream.length; i++) {
-      sum += stream[i];
-      if (i >= K)  sum -= stream[i - K];
-
-      // compute avg
-      if (i >= K - 1) {
-        avg[i - K + 1] = (int)(sum / K * 100) / 100.0;
-      }
-    }
-    return avg;
-  }
-
-  /** Get the max of the sliding window. */
-  public int[] max() {
-    int[] max = new int[stream.length - K + 1];
-    Deque<Integer> queue = new ArrayDeque<>();
-
-    for (int i = 0; i < K; i++) {
-      while (!queue.isEmpty() && stream[i] >= stream[queue.peekLast()])
-        queue.pollLast();
-      queue.add(i);
+        System.out.println(Arrays.toString(ary));
+        //    System.out.println("\t" + Arrays.toString(ins.average()));
+        System.out.println("\t" + Arrays.toString(ins.max()));
     }
 
-    for(int i = K; i < stream.length; i++){
-      max[i-K] = stream[queue.peekFirst()];
-      while (!queue.isEmpty() && stream[i] >= stream[queue.peekLast()])
-        queue.pollLast();
-      while(!queue.isEmpty() && queue.peekFirst() <= i - K)
-        queue.pollFirst();
-      queue.add(i);
+    /**
+     * Window size.
+     */
+    int K;
+    /**
+     * Array that simulates a stream.
+     */
+    int[] stream;
+
+    /**
+     * @param k      Window size.
+     * @param stream Array that simulates a stream.
+     */
+    public SlidingWindowStats(int k, int[] stream) {
+        this.K = k;
+        this.stream = stream;
     }
-    max[stream.length - K] = stream[queue.peekFirst()];
-    return max;
-  }
+
+    /**
+     * Get the average of the sliding window.
+     */
+    public double[] average() {
+        double sum = 0;
+        double[] avg = new double[stream.length - K + 1];
+        for (int i = 0; i < stream.length; i++) {
+            sum += stream[i];
+            if (i >= K)
+                sum -= stream[i - K];
+
+            // compute avg
+            if (i >= K - 1) {
+                avg[i - K + 1] = (int) (sum / K * 100) / 100.0;
+            }
+        }
+        return avg;
+    }
+
+    /**
+     * Get the max of the sliding window.
+     */
+    public int[] max() {
+        int[] max = new int[stream.length - K + 1];
+        Deque<Integer> queue = new ArrayDeque<>();
+
+    /* first window */
+        for (int i = 0; i < K; i++) {
+            while (!queue.isEmpty() && stream[i] >= stream[queue.peekLast()])
+                queue.pollLast();
+            queue.add(i);
+        }
+        System.out.println(queue);
+
+    /* following windows */
+        for (int i = K; i < stream.length; i++) {
+            max[i - K] = stream[queue.peekFirst()];
+            while (!queue.isEmpty() && stream[i] >= stream[queue.peekLast()])
+                queue.pollLast();
+            while (!queue.isEmpty() && queue.peekFirst() <= i - K)
+                queue.pollFirst();
+            queue.add(i);
+        }
+        max[stream.length - K] = stream[queue.peekFirst()];
+        return max;
+    }
 }
