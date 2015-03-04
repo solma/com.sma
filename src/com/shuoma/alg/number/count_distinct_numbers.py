b44a5418504635d44e826fbff50dc10021f61c60
solma <solma@motorola.com>
2015-03-03 16:42:47 -0800

    python reformat

--- src/com/shuoma/alg/number/count_distinct_numbers.py
+++ src/com/shuoma/alg/number/count_distinct_numbers.py
@@ -2,33 +2,33 @@ import random as rd
 import math as m
 
 
-N=1000000
-li=[]
+N = 1000000
+li = []
 for i in range(N):
     li.append(rd.randint(1, N))
 
 """
- direct hash 
+ direct hash
 """
-bm=[0]*int(m.ceil( N/8.0))
+bm = [0] * int(m.ceil(N / 8.0))
 for r in li:
-    bm[(r-1)/8] |= 0x1<< (r-1)%8
-cnt=0
-#print bm
+    bm[(r - 1) / 8] |= 0x1 << (r - 1) % 8
+cnt = 0
+# print bm
 for e in bm:
-    for i in range(8): 
-        if e>>i & 0x1 == 1:
-            cnt+=1
+    for i in range(8):
+        if e >> i & 0x1 == 1:
+            cnt += 1
 print cnt
 
 """
-sampling 
+sampling
 """
 import sys
-minimum=1
+
+minimum = 1
 for r in li:
-    minimum=min(minimum, r/(N-1.0))
-print 1/minimum-1
+    minimum = min(minimum, r / (N - 1.0))
+print 1 / minimum - 1
 
 
-        
--- src/com/shuoma/alg/string/expression.py
+++ src/com/shuoma/alg/string/expression.py
@@ -2,48 +2,50 @@ def shunting_yard(expr):
     """
      translate an inorder expression to reverse polish notation
     """
-    rpn=[]
-    stack=[]
-    priority={'+':1, '-':1, '*':2, '/':2, '(':5, ')':5}
+    rpn = []
+    stack = []
+    priority = {'+': 1, '-': 1, '*': 2, '/': 2, '(': 5, ')': 5}
     for c in expr:
         print c, stack, rpn
-        if ord(c)>=ord('0') and ord(c)<=ord('9'): #digit
+        if ord(c) >= ord('0') and ord(c) <= ord('9'):  # digit
             rpn.append(c)
         else:
-            if c!=')':
-                while len(stack)>0 and stack[-1]!='(' and priority[stack[-1]]>=priority[c] :
+            if c != ')':
+                while len(stack) > 0 and stack[-1] != '(' and priority[stack[-1]] >= priority[c]:
                     rpn.append(stack.pop())
                 stack.append(c)
             else:
-                while len(stack)>0 and stack[-1]!='(':
+                while len(stack) > 0 and stack[-1] != '(':
                     rpn.append(stack.pop())
                 stack.pop()
     rpn.extend(stack[::-1])
     return rpn
-    
+
+
 def eval(expr):
     """
       evaluate a reverse polish notation
     """
-    stack=[]
+    stack = []
     for c in expr:
-        if ord(c)>=ord('0') and ord(c)<=ord('9'): #digit
+        if ord(c) >= ord('0') and ord(c) <= ord('9'):  # digit
             stack.append(c)
         else:
             stack.append(calc(int(stack.pop()), int(stack.pop()), c))
     return int(stack[-1])
-    
+
+
 def calc(op1, op2, operator):
-    if operator=='+':
-        return op1+op2;
-    if operator=='-':
-        return op2-op1;
-    if operator=='*':
-        return op1*op2;
-    if operator=='/':
-        return op2/op1;
-        
+    if operator == '+':
+        return op1 + op2;
+    if operator == '-':
+        return op2 - op1;
+    if operator == '*':
+        return op1 * op2;
+    if operator == '/':
+        return op2 / op1;
+
+
 print eval(shunting_yard("(3+3/3*(1+3))-4/2"))
-    
-    
-    
+
+
