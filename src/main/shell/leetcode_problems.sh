aa65c5c9a680cccac93c713018c1f841bc2ef48d
solma <solma@motorola.com>
2015-03-27 15:19:22 -0700

    add more bash

--- src/main/shell/leetcode_problems.sh
+++ src/main/shell/leetcode_problems.sh
@@ -1,3 +1,4 @@
-# Word Frequency https://leetcode.com/problems/word-frequency/
+#!/usr/bin/env bash
+# 192 Word Frequency https://leetcode.com/problems/word-frequency/
 awk '{for(i=1;i<=NF;i++) a[$i]++} END {for(k in a) print k,a[k]}' words.txt | sort -k2 -nr
 
