7c5a0c16ecae7cbd1e7ee7c936d4450611d1d468
solma <solma@motorola.com>
2015-04-29 15:50:26 -0700

    fix travis

--- src/main/java/com/shuoma/alg/RemoveDuplicates.java
+++ src/main/java/com/shuoma/alg/RemoveDuplicates.java
@@ -1,31 +1,33 @@
-package com.shuoma.ds.linkedlist;
+package com.shuoma.alg;
 
 
 import static com.shuoma.annotation.Tag.DataStructure.LinkedList;
 import static com.shuoma.annotation.Tag.Source.CrackingTheCodeInterview;
 
 import com.shuoma.annotation.Tag;
+import com.shuoma.ds.linkedlist.*;
 
 // delete duplicates in the linked list without extra space
 @Tag(dss = LinkedList, source = CrackingTheCodeInterview)
 public class RemoveDuplicates {
   public static void main(String[] args) {
-    ListNode n = ListNode.buildList(new int[] {2, 0, 2, 1, 1, 1});
+    com.shuoma.ds.linkedlist.ListNode
+        n = com.shuoma.ds.linkedlist.ListNode.buildList(new int[] {2, 0, 2, 1, 1, 1});
     removeDuplicates(n);
     printList(n);
   }
 
-  static void printList(ListNode head) {
+  static void printList(com.shuoma.ds.linkedlist.ListNode head) {
     while (head != null) {
       System.out.println(head.val);
       head = head.next;
     }
   }
 
-  static void removeDuplicates(ListNode head) {
-    ListNode curHead = head;
+  static void removeDuplicates(com.shuoma.ds.linkedlist.ListNode head) {
+    com.shuoma.ds.linkedlist.ListNode curHead = head;
     while (curHead != null) {
-      ListNode prev = curHead, cur = curHead.next;
+      com.shuoma.ds.linkedlist.ListNode prev = curHead, cur = curHead.next;
       while (cur != null) {
         if (curHead.val == cur.val) {
           prev.next = cur.next; // delete duplicate
