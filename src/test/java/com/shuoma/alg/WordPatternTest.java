9593bb52cbc899f38215f9b1aee2a71bf74b650c
solma <shuoma86@gmail.com>
2015-10-11 19:25:14 -0700

    WordPattern

--- src/test/java/com/shuoma/alg/WordPatternTest.java
+++ src/test/java/com/shuoma/alg/WordPatternTest.java
@@ -1,16 +1,18 @@
 package com.shuoma.alg;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class WordPatternTest {
 
   @Test public void testMatch() throws Exception {
     WordPattern ins = new WordPattern();
-    assertEquals(true, ins.match("abba", "redbluebluered"));
-    assertEquals(true, ins.match("aaaa", "redredredred"));
-    assertEquals(false, ins.match("abab", "redbluebluered"));
-    assertEquals(false, ins.match("abba", "redredredred"));
+    assertEquals(true, ins.wordPattern("x", "redbluebluered"));
+    assertEquals(true, ins.wordPattern("abba", "red blue blue red"));
+    assertEquals(true, ins.wordPattern("abba", "red blue blue red"));
+    assertEquals(true, ins.wordPattern("aaaa", "red red red red"));
+    assertEquals(false, ins.wordPattern("abab", "red blue blue red"));
+    assertEquals(false, ins.wordPattern("abba", "red red red red"));
   }
 }
