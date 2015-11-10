c7b7b645df980844cec994f07c93d2278a20ddeb
solma <shuoma86@gmail.com>
2015-11-09 22:35:49 -0800

    range sum query

---
+++ src/main/python/alg/range_sum_query.py
@@ -0,0 +1,25 @@
+"""
+Given an integer array nums, find the sum of the elements between indices i and j (i<=j),inclusive.
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+"""
+
+from label import Label
+
+Label(Label.Array, Label.DynamicProgramming)
+
+
+class NumArray(object):
+    def __init__(self, nums):
+        self._sum = nums
+        for i in range(1, len(nums)):
+            self._sum[i] += self._sum[i - 1]
+
+    def sumRange(self, i, j):
+       return self._sum[j] - (0 if i == 0 else self._sum[i - 1])
---
+++ src/test/python/alg/num_range_sum_query_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from src.main.python.alg.range_sum_query import NumArray
+
+
+class TestNumArray(TestCase):
+    def test_sumRange(self):
+        na = NumArray([-2, 0, 3, -5, 2, -1])
+        self.assertEqual(1, na.sumRange(0, 2))
+        self.assertEqual(-1, na.sumRange(2, 5))
+        self.assertEqual(-3, na.sumRange(0, 5))
