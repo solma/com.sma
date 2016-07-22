95b2489fffb161b4c8f2dfab050e4ba2792583ce
Sol Ma <shuoma86@gmail.com>
2016-07-21 22:59:50 -0700

    bulk updates

--- src/main/java/com/sma/alg/PalindromePartitionII.java
+++ src/main/java/com/sma/alg/PalindromePartitionII.java
@@ -2,10 +2,13 @@ package com.sma.alg;
 
 import com.sma.annotation.Tag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.sma.annotation.Tag.Algorithm.DynamicProgramming;
+import static com.sma.annotation.Tag.Complexity.Quadratic;
 import static com.sma.annotation.Tag.DataStructure.StringT;
 import static com.sma.annotation.Tag.Reference.LeetCode;
-import static com.sma.annotation.Tag.Complexity.Quadratic;
 
 /**
  Given a string s, partition s such that every substring of the partition is a palindrome.
@@ -51,54 +54,58 @@ public class PalindromePartitionII {
 
 
   //TLE
-  // public void minCut(int l, int e, ArrayList<Integer> cuts, int[][] firstCutofMinCut){
-  // int cut;
-  // if(l<e&&firstCutofMinCut[l][e]>=0){
-  // cut=firstCutofMinCut[l][e];
-  // cuts.add(cut);
-  // //System.out.println(l+" "+e+"  "+cut);
-  // //minCut(0, cut, cuts, firstCutofMinCut);//logic will be incorrect with this line
-  // minCut(cut+1, e, cuts, firstCutofMinCut);
-  // }
-  // }
+  public void minCut1(int l, int e, List<Integer> cuts, int[][] firstCutofMinCut) {
+    int cut;
+    if (l < e && firstCutofMinCut[l][e] >= 0) {
+      cut = firstCutofMinCut[l][e];
+      cuts.add(cut);
+      //System.out.println(l+" "+e+"  "+cut);
+      //minCut(0, cut, cuts, firstCutofMinCut);//logic will be incorrect with this line
+      minCut1(cut + 1, e, cuts, firstCutofMinCut);
+    }
+  }
 
-  // public int minCut(String s){
-  // if(s==null||s.length()==0) return -1;
-  // int sLen=s.length();
-  // boolean [][] isPalindrome=new boolean[sLen][sLen];
-  // int i,len,j, k;
-  // for(i=0;i<sLen;i++) isPalindrome[i][i]=true;
-  // int[][] minCut=new int[sLen][sLen];
-  // int[][] firstCutofMinCut=new int[sLen][sLen];
+  public int minCut1(String s) {
+    if (s == null || s.length() == 0)
+      return -1;
+    int sLen = s.length();
+    boolean[][] isPalindrome = new boolean[sLen][sLen];
+    int i, len, j, k;
+    for (i = 0; i < sLen; i++)
+      isPalindrome[i][i] = true;
+    int[][] minCut = new int[sLen][sLen];
+    int[][] firstCutofMinCut = new int[sLen][sLen];
 
-  // for(i=0;i<sLen;i++)
-  // for(j=0;j<sLen;j++){
-  // if(i==j) minCut[i][j]=0;
-  // else minCut[i][j]=Integer.MAX_VALUE;
-  // firstCutofMinCut[i][j]=-1;
-  // }
+    for (i = 0; i < sLen; i++)
+      for (j = 0; j < sLen; j++) {
+        if (i == j)
+          minCut[i][j] = 0;
+        else
+          minCut[i][j] = Integer.MAX_VALUE;
+        firstCutofMinCut[i][j] = -1;
+      }
 
-  // boolean earlyEnd=false;
-  // for(len=1;len<=sLen;len++)
-  // for(i=0;i<=sLen-len;i++){
-  // j=i+len-1;
-  // //                System.out.println(i+" "+j+" "+len);
-  // if( (i+1>j-1 || isPalindrome[i+1][j-1] )&&s.charAt(i)==s.charAt(j)  ){
-  // isPalindrome[i][j]=true;
-  // minCut[i][j]=0;
-  // }
-  // //if(minCut[0][sLen-1]==0) {earlyEnd=true; break;}
-  // for(k=i;k<j;k++){
-  // if(minCut[i][k]+minCut[k+1][j]+1<minCut[i][j]){
-  // minCut[i][j]=minCut[i][k]+minCut[k+1][j]+1;
-  // firstCutofMinCut[i][j]=k;
-  // }
-  // }
-  // }
+    boolean earlyEnd = false;
+    for (len = 1; len <= sLen; len++)
+      for (i = 0; i <= sLen - len; i++) {
+        j = i + len - 1;
+        //                System.out.println(i+" "+j+" "+len);
+        if ((i + 1 > j - 1 || isPalindrome[i + 1][j - 1]) && s.charAt(i) == s.charAt(j)) {
+          isPalindrome[i][j] = true;
+          minCut[i][j] = 0;
+        }
+        //if(minCut[0][sLen-1]==0) {earlyEnd=true; break;}
+        for (k = i; k < j; k++) {
+          if (minCut[i][k] + minCut[k + 1][j] + 1 < minCut[i][j]) {
+            minCut[i][j] = minCut[i][k] + minCut[k + 1][j] + 1;
+            firstCutofMinCut[i][j] = k;
+          }
+        }
+      }
 
-  // ArrayList<Integer> cuts=new ArrayList<Integer>();
-  // minCut(0, sLen-1, cuts, firstCutofMinCut);
-  // System.out.println(cuts);
-  // return minCut[0][sLen-1];
-  // }
+    List<Integer> cuts = new ArrayList<>();
+    minCut1(0, sLen - 1, cuts, firstCutofMinCut);
+    System.out.println(cuts);
+    return minCut[0][sLen - 1];
+  }
 }
