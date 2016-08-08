a23c6cc6ab45faac6550de9876b4200a71937cf2
Sol Ma <solma@google.com>
2016-08-08 16:02:36 -0700

    switch to g++ for cpp compiler

--- src/main/cpp/lang/cpp11/lambda-function.cc
+++ src/main/cpp/lang/cpp11/lambda-function.cc
@@ -4,7 +4,7 @@ namespace {
   class MyClass {
     public:
       void run(const std::string& msg) const {
-        std::cout << "\"" << msg << "\" in member function run()" << std::endl;
+        std::cout << "  \"" << msg << "\" in member function run()" << std::endl;
       }
 
       void lambdaFunction() const {
@@ -19,13 +19,17 @@ namespace {
 
 int main(int argc, char** argv)
 {
+  std::cout << "lambda function that captures argc by value and argv by reference:" << std::endl;
   auto lf = [argc, &argv]() {
-    std::cout << "argc: " << argc << std::endl;
+    std::cout << "  argc: " << argc << std::endl;
     for (int arg = 0; arg < argc; arg++) {
-      std::cout << arg << " th arg: " << argv[arg] << std::endl;
+      std::cout << "  " << arg << "th arg: " << argv[arg] << std::endl;
     }
   };
   lf();
+  std::cout << std::endl;
+
+  std::cout << "lambda function in a class:" << std::endl;
   MyClass mc;
   mc.lambdaFunction();
 }
