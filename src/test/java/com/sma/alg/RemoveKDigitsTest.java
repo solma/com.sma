0d9f45123390571f58be09b1fffd2cd618109360
Sol Ma <shuoma86@gmail.com>
2016-09-26 16:28:21 -0700

    forgot the test file

--- src/test/java/com/sma/alg/RemoveKDigitsTest.java
+++ src/test/java/com/sma/alg/RemoveKDigitsTest.java
@@ -4,35 +4,31 @@ import com.sma.util.RandomUtil;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-import java.util.Arrays;
+import static com.sma.util.StringUtil.removePaddingCharacters;
 
 public class RemoveKDigitsTest extends TestCase {
 
   @Test
   public void testRemove() throws Exception {
-    int[] res = new int[3];
+    String[] res = new String[2];
     RemoveKDigits ins = new RemoveKDigits();
     for (int i = 0; i < 10000; i++) {
       int n = 123 + RandomUtil.r.nextInt(100000), k = 1 + RandomUtil.r.nextInt(3);
-      //int n = 69611, k = 3;
-      res[0] = removeDummy(n, k);
-      res[1] = ins.remove1(n, k);
-      res[2] = ins.remove(n, k);
+      String s = n + "";
+      res[0] = removeDummy(s, k);
+      res[1] = ins.remove(s, k);
       assertEquals(res[0], res[1]);
-      assertEquals(res[0], res[2]);
-      if (res[0] != res[2]) {
-        System.out.println(n + ":" + k + "   " + Arrays.toString(res));
-      }
     }
   }
 
-  int removeDummy(int n, int k) {
-    String s = String.valueOf(n);
-    if (k >= s.length()) return 0;
-    int min = n;
+  String removeDummy(String s, int k) {
+    if (k >= s.length()) return "0";
+    String min = s;
     for (String com : Combination.combinationsOfSizeNRecursionNOrderTopDown(s, s.length() - k)) {
-      min = Math.min(Integer.parseInt(com), min);
+      min = com.compareTo(min) < 0 ? com : min;
     }
-    return min;
+
+    if (min.length() == 0 || min.equals("0")) { return "0"; }
+    else { return removePaddingCharacters(min, '0', true); }
   }
 }
