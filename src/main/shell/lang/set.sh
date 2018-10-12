0f70ff8572d6a6a441e57ff9e066bf6821fc8dcc
Sol Ma <solma@solma.mtv.corp.google.com>
2018-10-12 16:46:52 -0700

    bulk updates

--- src/main/shell/lang/set.sh
+++ src/main/shell/lang/set.sh
@@ -10,12 +10,25 @@ function Set::-U() {
   set +u
 }
 
+function Set::-X() {
+  if [ "$1" != "xx" ]; then
+    set -x
+    echo $1
+  else
+    echo "input is xx"
+  fi
+  # do not print "set +x"
+  { set +x; } 2>/dev/null
+}
+
 function main() {
 #  Set::-U
 
   echo "null has no special meaning in bash."
   UNSET_VARIABLE=null
   Set::-U "${UNSET_VARIABLE}"
+
+  Set::-X "${UNSET_VARIABLE}"
 }
 
 main "$@"
