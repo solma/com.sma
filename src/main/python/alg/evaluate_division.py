8067b8343d63bc617054da6828f79d6c2649ecf5
Sol Ma <solma@google.com>
2020-12-05 12:21:51 -0800

    equivalence class

--- src/main/python/alg/evaluate_division.py
+++ src/main/python/alg/evaluate_division.py
@@ -15,65 +15,85 @@ This represents the equations. Return vector<double>.
 
 According to the example above:
 
-equations = [ ["a", "b"], ["b", "c"] ],
+equations = [ ['a', 'b'], ['b', 'c'] ],
 values = [2.0, 3.0],
-queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
-The input is always valid. You may assume that
-evaluating the queries will result in no division by zero and there is no contradiction.
+queries = [ ['a', 'c'], ['b', 'a'], ['a', 'e'], ['a', 'a'], ['x', 'x'] ].
+The input may be not valid, e.g. a/b = 2 and a/b = 3. If the input valid, return an error message.
+You may assume that evaluating the queries will result in no division by zero.
 """
 
 from alg.label import Label
 Label(Label.DirectedGraph, Label.UnionFind, Label.LeetCode)
 
 from collections import defaultdict
+
+_INVALID_INPUT_ERROR = 'InvalidInput'
+
+
 class EvaluateDivision(object):
   def calcEquation(self, equations, values, queries):
-    multiples = defaultdict(set)
     variables = {}
-    parent = {}
-
-    def get_cluster(c):
-      x = c
-      while x in parent:
-        x = parent[x]
-      return x
+    cluster_members = defaultdict(set)
+    cluster_lead = {}
 
     def assign_values():
       for i, eq in enumerate(equations):
         numerator, denominator = eq
-        parent[get_cluster(denominator)] = get_cluster(numerator)
-        multiples[denominator].add(numerator)
-        multiples[denominator].update(multiples[numerator])
 
-        if denominator not in variables:
-          if numerator not in variables:
-            variables[denominator] = 1
-            variables[numerator] = values[i]
+        if numerator not in cluster_lead and denominator not in cluster_lead:
+          cluster_lead[numerator] = numerator
+          cluster_lead[denominator] = numerator
+          variables[numerator] = values[i]
+          variables[denominator] = 1
+          cluster_members[numerator].add(numerator)
+          cluster_members[numerator].add(denominator)
+          continue
+        if numerator in cluster_lead and denominator not in cluster_lead:
+          lead = cluster_lead[numerator]
+          cluster_lead[denominator] = lead
+          cluster_members[lead].add(denominator)
+          variables[denominator] = variables[numerator] / values[i]
+          continue
+        if numerator not in cluster_lead and denominator in cluster_lead:
+          lead = cluster_lead[denominator]
+          cluster_lead[numerator] = lead
+          cluster_members[lead].add(numerator)
+          variables[numerator] = variables[denominator] * values[i]
+          continue
+        if numerator in cluster_lead and denominator in cluster_lead:
+          numerator_lead, denominator_lead = cluster_lead[numerator], cluster_lead[denominator]
+          if numerator_lead == denominator_lead:
+            if variables[numerator] != variables[denominator] * values[i]:
+              return _INVALID_INPUT_ERROR
+            else:
+              continue
+          # Merge two clusters
           else:
-            variables[denominator] = variables[numerator] / values[i]
-        else:
-          variables[numerator] = values[i] * variables[denominator]
-          for d in multiples[numerator]:
-            variables[d] *= values[i] * variables[denominator]
-        # print(variables, multiples)
+            scale_factor = (variables[numerator] / values[i]) / variables[denominator]
+            for member in cluster_members[denominator_lead]:
+              cluster_lead[member] = numerator_lead
+              cluster_members[numerator_lead].add(member)
+              variables[member] *= scale_factor
+            del cluster_members[denominator_lead]
 
     def evaluate():
       ret = []
       for q in queries:
         numerator, denominator = q
-        if numerator == denominator:
-          ret.append(1 if numerator in variables else -1)
-        elif get_cluster(numerator) != get_cluster(denominator):
-          ret.append(-1)
-        else:
+        if numerator in cluster_lead and denominator in cluster_lead and cluster_lead[numerator] == cluster_lead[denominator]:
           ret.append(variables[numerator] / variables[denominator])
+        else:
+          ret.append(-1)
       return ret
 
-    assign_values()
+    error_msg = assign_values()
+    if error_msg:
+      return error_msg
     return evaluate()
 
   def calcEquationGraph(self, equations, values, queries):
-    g = defaultdict(lambda: defaultdict(int))
+    g = defaultdict(lambda: defaultdict(float))
+    g_updated = defaultdict(lambda: defaultdict(bool))
     for (s, t), v in zip(equations, values):
       g[s][t] = v
       g[t][s] = 1.0 / v
@@ -82,31 +102,46 @@ class EvaluateDivision(object):
       for s in g:
         for t in g:
           if g[s][k] and g[k][t]:
-            g[s][t] = g[s][k] * g[k][t]
+            new_value = g[s][k] * g[k][t]
+            # if g_updated[s][t] and new_value != g[s][t]:
+            #   return _INVALID_INPUT_ERROR
+            g[s][t] = new_value
+            g_updated[s][t] = True
+
     ans = []
     for s, t in queries:
       ans.append(g[s][t] if g[s][t] else -1.0)
     return ans
 
+
 ins = EvaluateDivision()
-cases = [
-  ([["x1","x2"],["x2","x3"],["x1","x4"],["x2","x5"]],
-   [3.0,0.5,3.4,5.6],
-   [["x2","x4"],["x1","x5"],["x1","x3"],["x5","x5"],["x5","x1"],["x3","x4"],["x4","x3"],["x6","x6"],["x0","x0"]]),
-  ([["a","e"],["b","e"]],
-   [4.0,3.0],
-   [["a","b"],["e","e"],["x","x"]]),
-  ([["a","b"],["e","f"],["b","e"]], [3.4,1.4,2.3],
-   [["b","a"],["a","f"],["f","f"],["e","e"],["c","c"],["a","c"],["f","e"]])
+valid_cases = [
+  ([['x1', 'x2'], ['x2', 'x3'], ['x1', 'x4'], ['x2', 'x5']],
+   [3.0, 0.5, 3.4, 5.6],
+   [['x2', 'x4'], ['x1', 'x5'], ['x1', 'x3'], ['x5', 'x5'], ['x5', 'x1'], ['x3', 'x4'], ['x4', 'x3'], ['x6', 'x6'],
+    ['x0', 'x0']]),
+  ([['a', 'e'], ['b', 'e']],
+   [4.0, 3.0],
+   [['a', 'b'], ['e', 'e'], ['x', 'x']]),
+  ([['a', 'b'], ['e', 'f'], ['b', 'e']], [3, 2, 5],
+   [['b', 'a'], ['a', 'f'], ['f', 'f'], ['e', 'e'], ['c', 'c'], ['a', 'c'], ['f', 'e']]),
+]
+invalid_cases = [
+  ([['a', 'e'], ['b', 'e'], ['a', 'b']],
+   [4.0, 2.0, 3.],
+   [['a', 'b'], ['e', 'e'], ['x', 'x']]),
 ]
 
+
 import numpy
-for i, case in enumerate(cases[:]):
+
+for i, case in enumerate(valid_cases[:]):
   equations, values, queries = case[0], case[1], case[2]
-  ans1, ans2 = ins.calcEquationGraph(equations, values, queries), ins.calcEquation(equations, values, queries)
-  if not numpy.allclose(ans1, ans2):
+  ans1 = ins.calcEquation(equations, values, queries)
+  ans2 = ins.calcEquationGraph(equations, values, queries)
+  if ((ans1 != ans2 and (ans1 == _INVALID_INPUT_ERROR or ans2 == _INVALID_INPUT_ERROR))
+      or (not numpy.allclose(ans1, ans2))):
     print('case %s:\n ans1 = %s\n ans2 = %s' % (str(i), ans1, ans2))
 
-
-
-
+for case in invalid_cases:
+  print(ins.calcEquation(case[0], case[1], case[2]))
